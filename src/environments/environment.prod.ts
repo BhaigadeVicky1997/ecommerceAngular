167f786a9c420191477ed198e6ee0c1c7ca2f48b
VBhaigade <vbhaigade@kenmarkitan.com>
2020-10-20 23:03:14 +0530

    component,routing,navbar,bootstrap added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBRCe8_gK3XulIptTlo8B0z_IG9JaVDEJ8",
+    authDomain: "eshop-388f3.firebaseapp.com",
+    databaseURL: "https://eshop-388f3.firebaseio.com",
+    projectId: "eshop-388f3",
+    storageBucket: "eshop-388f3.appspot.com",
+    messagingSenderId: "705654919970",
+    appId: "1:705654919970:web:51b4a69e5c732f063b0b7b",
+    measurementId: "G-EHHPSYK6Z5"
+  }
 };
